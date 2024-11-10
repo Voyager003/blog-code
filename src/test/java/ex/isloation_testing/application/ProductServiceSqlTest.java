8282542e6c5911cd5c91330730f14217d963cf9b
voyager003 <miraclex000@gmail.com>
2024-11-10 17:20:06 +0900

    test: exercution pahse 적용

--- src/test/java/ex/isloation_testing/application/ProductServiceSqlTest.java
+++ src/test/java/ex/isloation_testing/application/ProductServiceSqlTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,7 +19,10 @@ class ProductServiceSqlTest {
     private ProductService productService;
 
     @Test
-    @Sql(scripts = {"/product/truncate.sql", "/product/ddl.sql"})
+    @Sql(value = "/product/ddl.sql",
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/product/truncate.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("전체 상품 목록을 조회한다")
     void getAllProducts() {
         List<Product> products = productService.getAllProducts();
@@ -33,7 +35,10 @@ class ProductServiceSqlTest {
     }
 
     @Test
-    @Sql(scripts = {"/product/truncate.sql", "/product/ddl.sql"})
+    @Sql(value = "/product/ddl.sql",
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/product/truncate.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("ID로 특정 상품을 조회한다")
     void getProductById() {
         // when
@@ -45,7 +50,10 @@ class ProductServiceSqlTest {
     }
 
     @Test
-    @Sql(scripts = {"/product/truncate.sql", "/product/ddl.sql"})
+    @Sql(value = "/product/ddl.sql",
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/product/truncate.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("존재하지 않는 상품 ID로 조회시 예외가 발생한다")
     void getProductById_NotFound() {
         assertThatThrownBy(() -> productService.getProductById(999L))
